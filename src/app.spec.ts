796e77cbda574c1335ab9f939941970e9ac2a1e1
Carlos Marchena <sprintwithcarlos@pm.me>
2022-07-07 20:04:58 +0200

    add swagger

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,7 +1,15 @@
+import supertest from "supertest";
 import http, { Server } from "http";
 import expressApp from "@/app";
 
 let server: Server;
+describe("Creates Swagger UI", () => {
+  it("should create swagger UI", async () => {
+    const request = supertest(expressApp.app);
+    const swaggerUIroute = await request.get("/api-docs");
+    expect(swaggerUIroute.status).toBe(301);
+  });
+});
 describe("Server Status", () => {
   beforeAll(() => {
     server = http.createServer(expressApp.app);
